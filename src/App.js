2c18ac451d1a3059919218efb725064dd1c431f0
Vicheka <phorvicheka@yahoo.com>
2020-09-16 15:58:40 +0900

    05-MasterProjectReactRouterAndRouting: 4. withRouter()
    How you navigate from MenuItem component to the HatsPage?
    
     * The Route component only pass the props (with properties history, ...) to the component that is routed to. For instance, the Route with attribute component={HomePage}, only HomePage component has access to the props (with properties history, ...) that the Route pass to. The Directory or MenuItem has no access to this props (with properties history, ...) even it is a child component of HomePage.
     * In order to navigate from MenuItem component to the HatsPage or to has access to the props (with properties history, ...), we can pass down that props from HomePage component to Directory component and down to MenuItem component. But, this is a bad pattern of prop drilling and time consuming. Passing down the props deep down to the children, is a bad pattern of prop drilling because the children in between for example the Directory component doesn't need that props.
     * react-router-dom provides us a high order component withRouter(): a function that take another component as an argument, transform it into another component and return that transformed component; solving this kind of props drilling.
    
    1/ Update App.js
    <Route exact path='/shop/hats' component={HatsPage} />
    
    2/ Using withRouter() of react-router-dom
    
    3/ using deconstructing props with spread operator ...

--- src/App.js
+++ src/App.js
@@ -3,38 +3,12 @@ import { Link, Route } from 'react-router-dom';
 
 import './App.css';
 
-//import HomePage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
-const HomePage = (props) => {
-    console.log(props);
+const HatsPage = (props) => {
     return (
         <div>
-            <Link to='/topics'>Topics</Link>
-            <button onClick={() => props.history.push('/topics')}>
-                Topics
-            </button>
-            <h1>HOME PAGE</h1>
-        </div>
-    );
-};
-
-const TopicList = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <h1>TOPIC LIST PAGE</h1>
-            <Link to={`${props.match.url}/15`}>TO TOPIC 15</Link> |
-            <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link> |
-            <Link to={`${props.match.url}/19`}>TO TOPIC 19</Link>
-        </div>
-    );
-};
-
-const TopicDetail = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <h1>TOPIC DETAIL PAGE {props.match.params.topicId}</h1>
+            <h1>HATS PAGE</h1>
         </div>
     );
 };
@@ -43,8 +17,7 @@ function App() {
     return (
         <div>
             <Route exact path='/' component={HomePage} />
-            <Route exact path='/topics' component={TopicList} />
-            <Route exact path='/topics/:topicId' component={TopicDetail} />
+            <Route exact path='/shop/hats' component={HatsPage} />
         </div>
     );
 }
