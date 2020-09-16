6117f1d76a5caeab14f4d18d475936ecf5f4a9a4
Vicheka <phorvicheka@yahoo.com>
2020-09-16 15:19:51 +0900

    05-MasterProjectReactRouterAndRouting: 3. React Router Dom
    * Route is high order component or wrapper component that will pass props pass to component
    <Route exact path='/' component={HomePage} />
    
    * Link
    <Link to='/topics'>Topics</Link>
    
    * From the props of the high order component Route, we can use props.history api to navigation between pages
    <button onClick={() => props.history.push('/topics')}>
            Topics
     </button>
    
    * Dynamic routing: props.match.url string
    <Link to={`${props.match.url}/15`}>TO TOPIC 15</Link> |
    <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link> |
     <Link to={`${props.match.url}/19`}>TO TOPIC 19</Link>
    
    * Route parameters: props.match.params object
    <h1>TOPIC DETAIL PAGE {props.match.params.topicId}</h1>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+//import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage = () => {
+const HomePage = (props) => {
+    console.log(props);
     return (
         <div>
-            <h1>HATS PAGE</h1>
+            <Link to='/topics'>Topics</Link>
+            <button onClick={() => props.history.push('/topics')}>
+                Topics
+            </button>
+            <h1>HOME PAGE</h1>
+        </div>
+    );
+};
+
+const TopicList = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>TOPIC LIST PAGE</h1>
+            <Link to={`${props.match.url}/15`}>TO TOPIC 15</Link> |
+            <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link> |
+            <Link to={`${props.match.url}/19`}>TO TOPIC 19</Link>
+        </div>
+    );
+};
+
+const TopicDetail = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>TOPIC DETAIL PAGE {props.match.params.topicId}</h1>
         </div>
     );
 };
@@ -16,10 +42,9 @@ const HatsPage = () => {
 function App() {
     return (
         <div>
-            <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route path='/hats' component={HatsPage} />
-            </Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/topics' component={TopicList} />
+            <Route exact path='/topics/:topicId' component={TopicDetail} />
         </div>
     );
 }
